36a09b929407e7564bbaeee3f3b187ac9eff5be5
michalkafka44 <93579718+michalkafka44@users.noreply.github.com>
2022-03-14 19:51:18 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,52 +1,47 @@
 #include "stm8s.h"
 #include "milis.h"
 
-//#include "delay.h"
-//#include <stdio.h>
-//#include "uart1.h"
+#include "delay.h"
+#include <stdio.h>
+#include "uart1.h"
 
 #define _ISOC99_SOURCE
 #define _GNU_SOURCE
-
-#define LED_PORT GPIOC
-#define LED_PIN  GPIO_PIN_5
-#define LED_HIGH   GPIO_WriteHigh(LED_PORT, LED_PIN)
-#define LED_LOW  GPIO_WriteLow(LED_PORT, LED_PIN)
-#define LED_REVERSE GPIO_WriteReverse(LED_PORT, LED_PIN)
-
-#define BTN_PORT GPIOE
-#define BTN_PIN  GPIO_PIN_4
-#define BTN_PUSH (GPIO_ReadInputPin(BTN_PORT, BTN_PIN)==RESET) 
 
 
 void setup(void)
 {
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
-    GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
-    GPIO_Init(BTN_PORT, BTN_PIN, GPIO_MODE_IN_FL_NO_IT);
+    GPIO_Init(GPIOD, GPIO_PIN_6, GPIO_MODE_IN_FL_NO_IT);
 
     init_milis();
-    //init_uart1();
+    init_uart1();
 }
 
+void delay_ms(uint16_t ms) {
+    uint16_t  i;
+    for (i=0; i<ms; i = i+1){
+        _delay_us(250);
+        _delay_us(248);
+        _delay_us(250);
+        _delay_us(250);
+    }
+}
 
 int main(void)
 {
-    uint32_t time = 0;
+    uint16_t pocet = 0;
 
     setup();
 
     while (1) {
 
-        if (milis() - time > 333 && BTN_PUSH) {
-            LED_REVERSE; 
-            time = milis();
-            //printf("%ld\n", time);
+        if(GPIO_ReadInputPin(GPIOD, GPIO_PIN_6)==RESET){
+            pocet += 1;
+            printf("aktualni pocet pruchodu je: %u\r\n",pocet);
+            delay_ms(500);
         }
 
-        //LED_REVERSE; 
-        //delay_ms(333);
-        //printf("Funguje to!!!\n");
     }
 }
 
